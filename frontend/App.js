e4f7510c4801d3a809bd9a04264eee929b90b5f5
Hardik <vijayvargiya.hardik@gmail.com>
2020-04-10 22:22:26 +0530

    Frontend in progress

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import Landing from './Components/Landing'
+
+const App = () => {
+    return(
+        <div>
+            <NavBar />
+            <Landing />
+        </div>
+    )
+}
---
+++ frontend/Components/Landing.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Landing = () => {
+    return(
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Build</b> a login/auth app with the{" "}
+              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
+              scratch
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              Let us Study Together
+            </p>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large btn-flat waves-effect white black-text"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Landing
